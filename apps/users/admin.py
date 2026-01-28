3a7743e293a0ae3fd575865e012ce245eaadc3e5
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2026-01-29 00:12:07 +0100

    Register the user model in the admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    """
+    Custom admin for the User model.
+
+    Displays email, first_name, last_name, and permission fields.
+    Provides search by email and name fields.
+    """
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_superuser",
+        "is_active"
+    )
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "first_name", "last_name", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_superuser", "is_active")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "first_name",
+                "last_name",
+                "password1",
+                "password2"
+            ),
+        }),
+    )
