46b68bc58b5125623f6eb167467240957221cc11
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2026-03-13 17:14:13 +0100

    Fix tests (full AI generation)

--- apps/factories/users.py
+++ apps/factories/users.py
@@ -6,8 +6,14 @@ from apps.users.models import User
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
+        skip_postgeneration_save = True
 
     email = factory.Sequence(lambda n: f"user{n}@example.com")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
-    password = factory.PostGenerationMethodCall("set_password", "testpass123")
+    @factory.post_generation
+    def password(obj, create, extracted, **kwargs):
+        password = extracted or "testpass123"
+        obj.set_password(password)
+        if create:
+            obj.save(update_fields=["password"])
